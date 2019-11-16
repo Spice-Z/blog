710ba24ce6361c58cac8f1df9e79a6b12c9e5393
Spice-Z <yugo.ogura@eure.jp>
2019-11-16 22:31:32 +0900

    各種設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `日々是関数`,
+    author: `Spice-Z`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
